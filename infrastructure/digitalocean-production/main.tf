41f3265d675b5c0af43748ffbe379edee112b593
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2025-06-23 17:45:03 -0300

    fix:  pipeline: update DigitalOcean production configuration

--- infrastructure/digitalocean-production/main.tf
+++ infrastructure/digitalocean-production/main.tf
@@ -16,18 +16,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket                      = "formerr-tfstate"
-    key                         = "digitalocean/production/terraform.tfstate"
-    endpoints = {
-      s3 = "https://formerr-spaces.nyc3.digitaloceanspaces.com"
-    }
-    region                      = "us-east-1" 
+    endpoint   = "https://formerr-spaces.nyc3.digitaloceanspaces.com"
+    bucket     = "formerr-spaces"
+    key        = "terraform/production/terraform.tfstate"
+    region     = "nyc3"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    force_path_style            = true
-    access_key                  = var.spaces_access_key
-    secret_key                  = var.spaces_secret_key
   }
 
 }
